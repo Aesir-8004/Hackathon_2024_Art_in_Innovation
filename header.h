20bda8ac0cecd47398d21a1b3ea011b883bf761b
Aesir-8004 <itliongivan@gmail.com>
2025-02-15 14:31:32 -0800

    Testing pushing header into repo

---
+++ header.h
@@ -0,0 +1,51 @@
+#ifndef HEADER_H
+#define HEADER_H
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+
+// Menu 
+
+void rules();
+
+void start();
+
+
+// Menu -> Settings
+void setting();
+
+int volume();
+
+// Game Stages
+
+
+
+int turtorial();
+
+int stage_1();
+
+int stage_2();
+
+int stage_3();
+
+
+// Game -> Functions
+
+int resultCompare();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
