8ae741d0e7fc55e7c064439d1607b04054b6957d
Sarah-1001 <137927364+Sarah-1001@users.noreply.github.com>
2025-02-15 18:49:15 -0800

    Update func.c

--- func.c
+++ func.c
@@ -1,5 +1,4 @@
 
-
 #include"header.h"
 
 int menu(void)
@@ -20,10 +19,10 @@ int menu(void)
 }
 void print_rules(void)
 {
-	printf("Hello! Before we get into drawing, here is some control you need to know.\n"); 
+	printf("Hello! Before we get into drawing, here is some control you need to know.\n");
 	putchar('\n');
-	printf("Main Controls:\n"); 
-	printf("------------------------\n"); 
+	printf("Main Controls:\n");
+	printf("------------------------\n");
 	printf("[Q][W][E]     [↖][↑][↗] \n");
 	printf("[A][S][D]  =  [←][ ][→] \n");
 	printf("[X][Z][C]     [↙][↓][↘] \n");
@@ -80,7 +79,7 @@ void print_rules(void)
 void start(Canvas theCanvas[], char symbol)
 {
 	int choise = 0;
-	printf("0.Tutorial\n1.Stage 1\n2.Stage 2\n3.Create\nMenu");
+	printf("0.Tutorial\n1.Stage 1\n2.Stage 2\n3.Create\n4.Menu");
 	scanf("%d", &choise);
 
 	switch (choise)
@@ -98,14 +97,17 @@ void start(Canvas theCanvas[], char symbol)
 		break;
 	case 3:
 		// Create
+		system("cls");
+		printCanvas(theCanvas, 0);
+
 		break;
 	case 4:
 		// Menu
-		system("cls"); 
+		system("cls");
 		menu();
 		break;
 	default:
-		printf("Error input"); 
+		printf("Error input");
 		break;
 	}
 }
@@ -121,18 +123,19 @@ void createCanvas(Canvas theCanvas[], char symbol, int first)
 			}
 		}
 	}
+
 }
 void printCanvas(Canvas theCanvas[], int first)
 {
 	int row_index = 0, col_index = 0;
 
 	printf("%5d%3d%3d%3d%3d%3d%3d%3d%3d%3d%4d%3d%3d%3d%3d%3d%3d%3d%3d%3d\n", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19);
-	for (row_index = 0; row_index < 20; ++row_index) 
+	for (row_index = 0; row_index < 20; ++row_index)
 	{
 		// Print the row number
 		printf("%-4d", row_index);
 
-		for (col_index = 0; col_index < 20; ++col_index) 
+		for (col_index = 0; col_index < 20; ++col_index)
 		{
 			// Print each cell in the row
 			printf("%-3c", theCanvas[first].canvasBoard[row_index][col_index]);
@@ -140,45 +143,55 @@ void printCanvas(Canvas theCanvas[], int first)
 		putchar('\n');
 	}
 }
+void clearCanvas(Canvas theCanvas[], int first)
+{
+	int row_index = 0, col_index = 0;
+	for (; row_index < 20; ++row_index) 
+	{
+		for (col_index = 0; col_index < 20; ++col_index) {
+			theCanvas[first].canvasBoard[row_index][col_index] = ' ';
+		}
+	}
+}
 
 int arr_length(char guide_array[])
 {
-// https://www.geeksforgeeks.org/length-of-array-in-c/
+	// https://www.geeksforgeeks.org/length-of-array-in-c/
 
-	int size = 0; 
+	int size = 0;
 	size = sizeof(guide_array) / sizeof(guide_array[0]);
 
-	return size; 
+	return size;
 }
 
 void print_what_to_input(char guide_array[], int length)
 {
 	for (int i = 0; i < length; i++)
 	{
-		printf("click %c !", guide_array[i]); 
+		printf("click %c !", guide_array[i]);
 	}
 }
 
 char ask_user_input(void)
 {
 	char user_input = '\n';
-	scanf("%c", &user_input); 
-	return user_input; 
+	scanf("%c", &user_input);
+	return user_input;
 }
 
-void check_mark(char canvas_array[][20], char guide_array[], char input_array[], int length, Coords point) 
+void check_mark(char canvas_array[][20], char guide_array[], char input_array[], int length, Coords point)
 {
-	
+
 	for (int i = 0; i < length; i++)
 	{
-		input_array[i] = ask_user_input(); 
+		input_array[i] = ask_user_input();
 
-		if (input_array[i] == 'W') 
+		if (input_array[i] == 'W')
 		{
 			//vert 
 			if (input_array[i] == guide_array[i])
-			{ 
-				update_mark(canvas_array, input_array, length, point);  
+			{
+				update_mark(canvas_array, input_array, length, point);
 			}
 			else
 			{
@@ -197,7 +210,7 @@ void check_mark(char canvas_array[][20], char guide_array[], char input_array[],
 				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'Z') 
+		else if (input_array[i] == 'Z')
 		{
 			// vert
 			if (input_array[i] == guide_array[i])
@@ -209,7 +222,7 @@ void check_mark(char canvas_array[][20], char guide_array[], char input_array[],
 				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'D') 
+		else if (input_array[i] == 'D')
 		{
 			// hor
 			if (input_array[i] == guide_array[i])
@@ -221,7 +234,7 @@ void check_mark(char canvas_array[][20], char guide_array[], char input_array[],
 				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'Q') 
+		else if (input_array[i] == 'Q')
 		{
 			//dia
 			if (input_array[i] == guide_array[i])
@@ -233,36 +246,36 @@ void check_mark(char canvas_array[][20], char guide_array[], char input_array[],
 				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'E') 
+		else if (input_array[i] == 'E')
 		{
 			//dia
 			if (input_array[i] == guide_array[i])
 			{
-				update_mark(canvas_array, input_array, length, point); 
+				update_mark(canvas_array, input_array, length, point);
 			}
 			else
 			{
-				update_mark(canvas_array, input_array, length, point); 
+				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'X') 
+		else if (input_array[i] == 'X')
 		{
 			//dia
 			if (input_array[i] == guide_array[i])
 			{
-				update_mark(canvas_array, input_array, length, point); 
+				update_mark(canvas_array, input_array, length, point);
 			}
 			else
 			{
-				update_mark(canvas_array, input_array, length, point); 
+				update_mark(canvas_array, input_array, length, point);
 			}
 		}
-		else if (input_array[i] == 'C') 
+		else if (input_array[i] == 'C')
 		{
 			//dia
-			if (input_array[i] == guide_array[i]) 
+			if (input_array[i] == guide_array[i])
 			{
-				update_mark(canvas_array, input_array, length, point); 
+				update_mark(canvas_array, input_array, length, point);
 			}
 			else
 			{
@@ -281,8 +294,8 @@ void update_mark(char canvas_array[][20], char input_array[], int length, Coords
 		if (input_array[i] == 'W')
 		{
 			// vert
-			canvas_array[point.rows][point.cols] = '|'; 
-			point.rows += 1;  
+			canvas_array[point.rows][point.cols] = '|';
+			point.rows += 1;
 			canvas_array[point.rows][point.cols] = '|';
 		}
 		else if (input_array[i] == 'A')
@@ -303,7 +316,7 @@ void update_mark(char canvas_array[][20], char input_array[], int length, Coords
 		{
 			// hor
 			canvas_array[point.rows][point.cols] = '-';
-			point.cols -= 1; 
+			point.cols -= 1;
 			canvas_array[point.cols][point.rows] = '-';
 		}
 		else if (input_array[i] == 'Q')
@@ -331,26 +344,17 @@ void update_mark(char canvas_array[][20], char input_array[], int length, Coords
 			point.cols += 1;
 			canvas_array[point.rows][point.cols] = '/';
 			point.cols -= 1;
-			point.rows += 1; 
+			point.rows += 1;
 			canvas_array[point.cols][point.rows] = '/';
 		}
 		else if (input_array[i] == 'C')
 		{
 			//dia
 			canvas_array[point.rows][point.cols] = '\\';
-			point.rows += 1; 
+			point.rows += 1;
 			point.cols += 1;
-			canvas_array[point.cols][point.rows] = '\\';
+			canvas_array[point.cols][point.rows] = '\\'; 
 
-		}
-	}
-}
-void clearCanvas(Canvas theCanvas[], int first)
-{
-	int row_index = 0, col_index = 0;
-	for (; row_index < 20; ++row_index) {
-		for (col_index = 0; col_index < 20; ++col_index) {
-			theCanvas[first].canvasBoard[row_index][col_index] = '-';
 		}
 	}
 }
