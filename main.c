ca407ac907efa6304121bd896bec48ed74afccf0
Aesir-8004 <itliongivan@gmail.com>
2025-02-16 03:28:36 -0800

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ Canvas canvasBoard[10];
 char symbol = ' ';
 char tutorial_arr[] = {'D','D','C','C','X','X','Z','Z','A','A','Q','Q','W','W','E','O'};
 char stage1_arr[18] = { 'W','W','W','E','C','X','D','W','E','C','X','X','X','A','A','A','A','A' }; 
-char stage2_arr[] = { 'd','e','c','d','x','z','q','x','z','q','x','w','q','*','*' };
+char stage2_arr[] = { 'D','D','E','E','C','C','D','D','Z','Z','X','X','Q','Q','Z','Z','W','W','Q','*' };
 
 
 createCanvas(canvasBoard, symbol, 0);
